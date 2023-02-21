5a27eae83dfb2cb9e5cc00658db82add2c036de6
Oleksii Moisieiev <oleksii_moisieiev@epam.com>
2023-02-21 19:14:21 +0200

    xen-dom-mgmt: Generate device tree for the Domain
    Use libfdt module to generate the device tree.
    This is required to generate the device-tree for the
    Domains.
    Library generates all basic nodes for the Guest Domain and
    supports GICv2/GiCv3 switch and optee as optionally.
    Partial device tree /passthrough and /aliases should be copied
    to the Domain device tree.
    If XEN_LIBFDT config parameter was disabled - provided pfdt will be
    passed to the Domain without changes.
    
    Application should be configured with the following param:
    CONFIG_XEN_LIBFDT=y
    
    Signed-off-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>
    Acked-by: Dmytro Firsov <dmytro_firsov@epam.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

---
+++ xen-dom-mgmt/include/xen-dom-fdt.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright (c) 2023 EPAM Systems
+ */
+
+#ifndef XENLIB_XEN_DOM_FDT_H
+#define XENLIB_XEN_DOM_FDT_H
+
+#include <domain.h>
+
+int gen_domain_fdt(struct xen_domain_cfg *domcfg, void **fdtaddr,
+		size_t *fdtsize, int xen_major, int xen_minor, void *pfdt,
+				   size_t pfdt_size, int domid);
+
+#endif /* XENLIB_XEN_DOM_FDT_H */
