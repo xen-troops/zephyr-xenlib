2cfaa845db12afa9b5dcf4eeb1cf2a9d06c87cd2
Oleksii Moisieiev <oleksii_moisieiev@epam.com>
2023-02-22 10:29:29 +0200

    xen_cmds: Add domu_create functions as a shell commands
    Xenlib provides xu command which is intended to start predefined
    domains, based on it domid. Rewriting this command to use common
    domain-control interface
    
    Signed-off-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>
    Reviewed-by: Dmytro Firsov <dmytro_firsov@epam.com>

--- xen-shell-cmd/include/xen_shell.h
+++ xen-shell-cmd/include/xen_shell.h
@@ -6,13 +6,6 @@
 #ifndef XENLIB_XEN_SHELL_H
 #define XENLIB_XEN_SHELL_H
 
-int domu_console_start(const struct shell *shell, size_t argc, char **argv);
-int domu_console_stop(const struct shell *shell, size_t argc, char **argv);
-int domu_create(const struct shell *shell, size_t argc, char **argv);
-int domu_destroy(const struct shell *shell, size_t argc, char **argv);
-int domu_pause(const struct shell *shell, size_t argc, char **argv);
-int domu_unpause(const struct shell *shell, size_t argc, char **argv);
-
 /*
  * Initialize domain console by setting HVM param for domain
  * and event channel binding in dom0.
