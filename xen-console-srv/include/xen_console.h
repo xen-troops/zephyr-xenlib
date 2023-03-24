ae9c21608cb8f19d499678181084de4bccc313cc
Volodymyr Babchuk <volodymyr_babchuk@epam.com>
2023-03-24 17:16:52 +0200

    xen_console: implement shell interaction with domain console
    This commit adds xen_attach_domain_console() function that can be used
    to attach to a domain console. It works in the following way:
    
    1. Due to Xen Shell API limitations we can't pass private data to
       console shell call-back. Thus, we need to store attached console
       in a global variable `current_console`. It is protected by
       `global_console_lock`.
    
    2. Then we spawn reader thread. It will read data from an internal
       ring buffer of a given console and display data via Shell API
    
    3. We register shell bypass callback. This callback will receive
       all user input and forward it to a DomU. Also it looks for escape
       character ("CTRL+]", to be compatible with xen-tools) and initiates
       detachment process.
    
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Acked-by: Mykola Kvach <mykola_kvach@epam.com>
    Acked-by: Leonid Komarianskyi <leonid_komarianskyi@epam.com>
    Reviewed-by: Dmytro Firsov <dmytro_firsov@epam.com>

--- xen-console-srv/include/xen_console.h
+++ xen-console-srv/include/xen_console.h
@@ -6,6 +6,8 @@
 #ifndef XENLIB_XEN_CONSOLE_H
 #define XENLIB_XEN_CONSOLE_H
 
+#include <zephyr/shell/shell.h>
+
 /*
  * Initialize domain console by setting HVM param for domain
  * and event channel binding in dom0.
@@ -33,6 +35,18 @@ int xen_start_domain_console(struct xen_domain *domain);
  * @return - zero on success, negative errno on failure
  */
 int xen_stop_domain_console(struct xen_domain *domain);
+
+/*
+ * Attach Zephyr shell to console in given domain
+ *
+ * @param shell - Zephyr shell instance attach to
+ *
+ * @param domain - domain, which console should be attached
+ *
+ * @return - zero on success, negative errno on failure
+ */
+int xen_attach_domain_console(const struct shell *shell,
+			      struct xen_domain *domain);
 
 #endif /* XENLIB_XEN_CONSOLE_H */
 
