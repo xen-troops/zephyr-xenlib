eaf8fd2e4ab9655048f84646df0677cd6b12da98
Leonid Komarianskyi <leonid_komarianskyi@epam.com>
2023-04-19 13:12:39 +0300

    xenstore-srv: move private structures from header to c file
    Several structures are private and used only in one file. So,
    they should be placed in C file.
    
    Reviewed-by: Dmytro Firsov <dmytro_firsov@epam.com>
    Acked-by: Mykola Kvach <mykola_kvach@epam.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Signed-off-by: Leonid Komarianskyi <leonid_komarianskyi@epam.com>

--- xenstore-srv/include/xenstore_srv.h
+++ xenstore-srv/include/xenstore_srv.h
@@ -7,30 +7,6 @@
 #ifndef XENLIB_XENSTORE_SRV_H
 #define XENLIB_XENSTORE_SRV_H
 
-struct xs_entry {
-	char *key;
-	char *value;
-	sys_dlist_t child_list;
-
-	sys_dnode_t node;
-};
-
-struct watch_entry {
-	char *key;
-	char *token;
-	struct xen_domain *domain;
-	bool is_relative;
-
-	sys_dnode_t node;
-};
-
-struct pending_watch_event_entry {
-	char *key;
-	struct xen_domain *domain;
-
-	sys_dnode_t node;
-};
-
 int start_domain_stored(struct xen_domain *domain);
 int stop_domain_stored(struct xen_domain *domain);
 
