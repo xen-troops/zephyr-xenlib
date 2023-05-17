46087322fb449708a82571ce45b3c023b1c9be5e
Leonid Komarianskyi <leonid_komarianskyi@epam.com>
2023-05-17 12:16:55 +0300

    xenstore: move xenstore-related fields to separate structure
    Creating separate structure "struct xenstore" is needed for
    several reasons:
    
    1. Further changes require to extend the number of xenstore
       related fields and it's better to store it in the separate
       structure in order to not "overload" domain structure;
    
    2. Some fields from domain structure such as domint or transaction
       are not informative on how they are related to xenstore.
       So it's worth moving them to separate structure;
    
    Please note that some nesting field changes in functions are made
    for keeping code buildable consistency but they can be removed in
    further patches.
    
    Reviewed-by: Dmytro Firsov <dmytro_firsov@epam.com>
    Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Signed-off-by: Leonid Komarianskyi <leonid_komarianskyi@epam.com>

--- xenstore-srv/include/xenstore_srv.h
+++ xenstore-srv/include/xenstore_srv.h
@@ -7,6 +7,21 @@
 #ifndef XENLIB_XENSTORE_SRV_H
 #define XENLIB_XENSTORE_SRV_H
 
+struct xenstore {
+	struct xenstore_domain_interface *domint;
+	struct xen_domain *domain;
+	struct k_sem xb_sem;
+	struct k_thread thrd;
+	atomic_t thrd_stop;
+	evtchn_port_t remote_evtchn;
+	evtchn_port_t local_evtchn;
+	size_t xs_stack_slot;
+	int transaction;
+	int running_transaction;
+	int stop_transaction_id;
+	bool pending_stop_transaction;
+};
+
 int start_domain_stored(struct xen_domain *domain);
 int stop_domain_stored(struct xen_domain *domain);
 
