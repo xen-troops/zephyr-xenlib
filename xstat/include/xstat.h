08ca426f9af1d5d5739c42edf8d06ddf7c967a0b
Mykyta Poturai <mykyta_poturai@epam.com>
2024-04-30 10:36:01 +0300

    xstat: Add doxygen comments for public functions
    Document all of the public functions in the xstat API with doxygen
    
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Reviewed-by: Grygorii Strashko <grygorii_strashko@epam.com>
    Acked-by: Dmytro Firsov <dmytro_firsov@epam.com>

--- xstat/include/xstat.h
+++ xstat/include/xstat.h
@@ -47,8 +47,32 @@ struct xenstat {
 	char xen_version[VERSION_SIZE];	/* xen version running on this node */
 };
 
+/**
+ * Retrieves the Xen statistics for the specified domain.
+ *
+ * @param stat The pointer to the struct where the Xen statistics will be stored.
+ * @return 0 on success, or a negative error code on failure.
+ */
 int xstat_getstat(struct xenstat *stat);
+
+/**
+ * Retrieves information about the specified domains.
+ *
+ * @param info The pointer to the struct where the domain information will be stored.
+ * @param first The ID of the first domain to retrieve information for.
+ * @param num The maximum number of domains to retrieve information for.
+ * @return 0 on success, or a negative error code on failure.
+ */
 int xstat_getdominfo(struct xenstat_domain *info, uint16_t first, uint16_t num);
+
+/**
+ * Retrieves information about the specified VCPU of a domain.
+ *
+ * @param info The pointer to the struct where the VCPU information will be stored.
+ * @param dom The ID of the domain.
+ * @param vcpu The ID of the VCPU.
+ * @return 0 on success, or a negative error code on failure.
+ */
 int xstat_getvcpu(struct xenstat_vcpu *info, uint16_t dom, uint16_t vcpu);
 
 #ifdef __cplusplus
