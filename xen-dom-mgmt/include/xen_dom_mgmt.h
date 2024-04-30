77c78ba21e55fb71c1fd4c490b13d64bd2d97c61
Mykyta Poturai <mykyta_poturai@epam.com>
2024-04-30 10:42:31 +0300

    xen-dom-mgmt: Add doxygen comments for public functions
    Document all of the public functions in the xen-dom-mgmt API with
    doxygen
    
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Reviewed-by: Grygorii Strashko <grygorii_strashko@epam.com>
    Acked-by: Dmytro Firsov <dmytro_firsov@epam.com>

--- xen-dom-mgmt/include/xen_dom_mgmt.h
+++ xen-dom-mgmt/include/xen_dom_mgmt.h
@@ -13,10 +13,47 @@
 extern "C" {
 #endif
 
+
+/**
+ * Creates a new domain with the specified configuration.
+ *
+ * @param domcfg The configuration for the new domain.
+ * @param domid The ID of the new domain.
+ * @return 0 on success, or an error code on failure.
+ */
 int domain_create(struct xen_domain_cfg *domcfg, uint32_t domid);
+
+/**
+ * Destroys the specified domain.
+ *
+ * @param domid The ID of the domain to destroy.
+ * @return 0 on success, or an error code on failure.
+ */
 int domain_destroy(uint32_t domid);
+
+/**
+ * Pauses the specified domain.
+ *
+ * @param domid The ID of the domain to pause.
+ * @return 0 on success, or an error code on failure.
+ */
 int domain_pause(uint32_t domid);
+
+/**
+ * Unpauses the specified domain.
+ *
+ * @param domid The ID of the domain to unpause.
+ * @return 0 on success, or an error code on failure.
+ */
 int domain_unpause(uint32_t domid);
+
+/**
+ * Performs post-creation operations for the specified domain.
+ *
+ * @param domcfg The configuration for the domain.
+ * @param domid The ID of the domain.
+ * @return 0 on success, or an error code on failure.
+ */
 int domain_post_create(const struct xen_domain_cfg *domcfg, uint32_t domid);
 
 #ifdef __cplusplus
