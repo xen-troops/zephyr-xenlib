e4f9603d10b5ef8717897be52d0b18a0e76a67d9
Mykyta Poturai <mykyta_poturai@epam.com>
2024-05-01 09:55:30 +0300

    xen-dom-xs: Add doxygen comments for public functions
    Document all of the public functions in the xen-dom-xs API with
    doxygen
    
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Reviewed-by: Grygorii Strashko <grygorii_strashko@epam.com>
    Acked-by: Dmytro Firsov <dmytro_firsov@epam.com>

--- xen-dom-mgmt/include/xen-dom-xs.h
+++ xen-dom-mgmt/include/xen-dom-xs.h
@@ -8,10 +8,47 @@
 #ifndef XENLIB_XEN_DOM_XS_H
 #define XENLIB_XEN_DOM_XS_H
 
+/**
+ * Deinitializes the XenStore for a specific domain.
+ *
+ * @param domid The ID of the domain.
+ */
 void xs_deinitialize_domain_xenstore(uint32_t domid);
+
+/**
+ * Adds a PV block backend to the XenStore for a specific domain.
+ *
+ * @param cfg The configuration of the PV block backend.
+ * @param domid The ID of the domain.
+ * @return 0 on success, negative error code on failure.
+ */
 int xs_add_pvblock_xenstore(const struct pv_block_configuration *cfg, int domid);
+
+/**
+ * Removes all XenStore backends for a specific domain.
+ *
+ * @param domid The ID of the domain.
+ * @return 0 on success, negative error code on failure.
+ */
 int xs_remove_xenstore_backends(int domid);
+
+/**
+ * Adds a PV network backend to the XenStore for a specific domain.
+ *
+ * @param cfg The configuration of the PV network backend.
+ * @param domid The ID of the domain.
+ * @param instance_id The instance ID of the PV network backend.
+ * @return 0 on success, negative error code on failure.
+ */
 int xs_add_pvnet_xenstore(const struct pv_net_configuration *cfg, int domid, int instance_id);
+
+/**
+ * Initializes the XenStore for a specific domain.
+ *
+ * @param domid The ID of the domain.
+ * @param domain The Xen domain structure.
+ * @return 0 on success, negative error code on failure.
+ */
 int xs_initialize_xenstore(uint32_t domid, const struct xen_domain *domain);
 
 #endif /* XENLIB_XEN_DOM_XS_H */
