3fb1f02c6b4b08cc04550b936b740909aabb0c7c
Mykyta Poturai <mykyta_poturai@epam.com>
2024-05-01 09:48:30 +0300

    xen-dom-fdt: Add doxygen comments for public functions
    Document all of the public functions in the xen-dom-fdt API with
    doxygen
    
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Reviewed-by: Grygorii Strashko <grygorii_strashko@epam.com>
    Acked-by: Dmytro Firsov <dmytro_firsov@epam.com>

--- xen-dom-mgmt/include/xen-dom-fdt.h
+++ xen-dom-mgmt/include/xen-dom-fdt.h
@@ -12,10 +12,28 @@
 extern "C" {
 #endif
 
+/**
+ * Generates the device tree blob (FDT) for a Xen domain.
+ *
+ * @param domcfg The domain configuration.
+ * @param fdtaddr Pointer to store the address of the generated FDT.
+ * @param fdtsize Pointer to store the size of the generated FDT.
+ * @param xen_major The major version of Xen.
+ * @param xen_minor The minor version of Xen.
+ * @param pfdt Pointer to a partial FDT
+ * @param pfdt_size The size of the partial FDT
+ * @param domid The domain ID.
+ * @return Returns 0 on success, or a negative error code on failure.
+ */
 int gen_domain_fdt(struct xen_domain_cfg *domcfg, void **fdtaddr,
 		size_t *fdtsize, int xen_major, int xen_minor, void *pfdt,
 		size_t pfdt_size, int domid);
 
+/**
+ * Frees the memory allocated for a device tree blob (FDT).
+ *
+ * @param fdt Pointer to the FDT to be freed.
+ */
 void free_domain_fdt(void *fdt);
 
 #ifdef __cplusplus
