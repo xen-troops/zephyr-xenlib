f45b782e704361854591453eb9735940843f1d52
Grygorii Strashko <grygorii_strashko@epam.com>
2024-05-05 15:13:58 +0300

    xenlib: add sphinx/doxygen docs and build
    This patch adds initial version of xenlib documantation which is built
    using sphinx and doxygen.
    
    to build docs run below commands:
     cd docs
     mkdir build
     cd build
     cmake ../
     make [Doxygen|Sphinx]
    
    The HTML docs can be found in build/sphinx/index.html.
    
    The patch is based on [1] and [2], and most of doxygen and sphinx settings
    just taken form Zephyr docs.
    
    [1] https://devblogs.microsoft.com/cppblog/clear-functional-c-documentation-with-sphinx-breathe-doxygen-cmake/
    [2] https://medium.com/practical-coding/c-documentation-with-doxygen-cmake-sphinx-breathe-for-those-of-use-who-are-totally-lost-part-2-21f4fb1abd9f
    Signed-off-by: Grygorii Strashko <grygorii_strashko@epam.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Acked-by: Dmytro Firsov <dmytro_firsov@epam.com>

---
+++ docs/FindSphinx.cmake
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2024 EPAM Systems
+
+#Look for an executable called sphinx-build
+find_program(SPHINX_EXECUTABLE
+             NAMES sphinx-build
+             DOC "Path to sphinx-build executable")
+
+include(FindPackageHandleStandardArgs)
+
+#Handle standard arguments to find_package like REQUIRED and QUIET
+find_package_handle_standard_args(Sphinx
+                                  "Failed to find sphinx-build executable"
+                                  SPHINX_EXECUTABLE)
---
+++ docs/index.rst
@@ -0,0 +1,26 @@
+.. xenlib_docs
+
+zephyr-xenlib documentation
+===========================
+
+.. toctree::
+   :maxdepth: 3
+   :caption: Contents:
+
+
+.. mdinclude:: ../README.md
+
+Table of Contents
+=================
+
+.. toctree::
+
+   self
+   api/index
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
