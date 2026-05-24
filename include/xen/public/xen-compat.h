58bd4bf93f6009f01fc9ce0ef498e62153192b00
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2026-05-24 09:11:28 +0900

    include: Adapt xen-compat.h for zephyr
    - Define __XEN_TOOLS__ if CONFIG_XEN_DOM0 enabled
    - Define __XEN_INTERFACE_VERSION__ from CONFIG_XEN_INTERFACE_VERSION
    - Add the common include files needed by the Xen public headers
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>
    Reviewed-by: Dmytro Firsov <dmytro_firsov@epam.com>
    Reviewed-by: Mykola Kvach <mykola_kvach@epam.com>
    Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- include/xen/public/xen-compat.h
+++ include/xen/public/xen-compat.h
@@ -5,6 +5,7 @@
  * Guest OS interface to Xen.  Compatibility layer.
  *
  * Copyright (c) 2006, Christian Limpach
+ * Copyright (c) 2026, TOKITA Hiroshi
  */
 
 #ifndef __XEN_PUBLIC_XEN_COMPAT_H__
@@ -12,9 +13,10 @@
 
 #define __XEN_LATEST_INTERFACE_VERSION__ 0x00041300
 
-#if defined(__XEN__) || defined(__XEN_TOOLS__)
+#ifdef CONFIG_XEN_DOM0
+#define __XEN_TOOLS__
 /* Xen is built with matching headers and implements the latest interface. */
-#define __XEN_INTERFACE_VERSION__ __XEN_LATEST_INTERFACE_VERSION__
+#define __XEN_INTERFACE_VERSION__ CONFIG_XEN_INTERFACE_VERSION
 #elif !defined(__XEN_INTERFACE_VERSION__)
 /* Guests which do not specify a version get the legacy interface. */
 #define __XEN_INTERFACE_VERSION__ 0x00000000
@@ -25,5 +27,13 @@
 #endif
 
 #define COMPAT_FLEX_ARRAY_DIM XEN_FLEX_ARRAY_DIM
+
+#if defined(CONFIG_XEN)
+#if !defined(_ASMLANGUAGE)
+/* modification for zephyr: Add common definitions. */
+#include <stdbool.h>
+#include <stdint.h>
+#endif
+#endif
 
 #endif /* __XEN_PUBLIC_XEN_COMPAT_H__ */
